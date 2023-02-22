162b0f742feb27df2105dfd320b612288caf7244
aryankhatana01 <aryankhatana01@gmail.com>
2023-02-23 00:23:19 +0530

    changed hyperparameters for training

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ with open("input/ix2tok.json") as f:
     vocab = json.load(f)
 
 class CFG:
-    batch_size = 16 # how many independent sequences will we process in parallel?
+    batch_size = 64 # how many independent sequences will we process in parallel?
     block_size = 256 # what is the maximum context length for predictions?
     max_iters = 5000
     eval_interval = 5
@@ -13,7 +13,7 @@ class CFG:
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     eval_iters = 200
     n_embd = 384
-    n_head = 2
-    n_layer = 2
+    n_head = 6
+    n_layer = 4
     dropout = 0.2
     vocab_size = len(vocab)

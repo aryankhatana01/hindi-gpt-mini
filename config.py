dae0a78411d794871642f09f9e49ea53ce8c27d0
aryankhatana01 <aryankhatana01@gmail.com>
2023-02-22 22:26:29 +0530

    created a config file for the model

---
+++ config.py
@@ -0,0 +1,14 @@
+import torch
+
+class CFG:
+    batch_size = 16 # how many independent sequences will we process in parallel?
+    block_size = 256 # what is the maximum context length for predictions?
+    max_iters = 5000
+    eval_interval = 5
+    learning_rate = 3e-4
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    eval_iters = 200
+    n_embd = 384
+    n_head = 2
+    n_layer = 2
+    dropout = 0.2
